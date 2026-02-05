ea09fc63ccf732d3dd6240fe2923d8490034474f
Sandrin Muramutsa <sandrinmuramutsa@gmail.com>
2026-02-04 20:48:00 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by sandrine Bakuramutsa on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
